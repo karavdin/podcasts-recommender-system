245ab197954b34bc07c7737ff10e9bf8f2cb7c48
Anastasia Karavdina <anastasiakaravdina@AnastasasLaptop.fritz.box>
2024-08-03 08:43:53 +0200

    cleaning

--- .ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,5 +0,0 @@
-PODCASTS_DATABASE_PATH_RAW='/data/raw/podcasts.csv'
-USER_REVIEWS_DATABASE_PATH_RAW='/data/raw/user_reviews.csv'
-
-PODCASTS_DATABASE_PATH_PROCESSED='/data/processed'
-USER_REVIEWS_DATABASE_PATH_PROCESSED='/data/processed'
