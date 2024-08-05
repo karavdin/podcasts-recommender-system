c000fd7f4e2758bebd851727973699dd15205c23
karavdin <karavdina@gmail.com>
2024-08-05 06:52:24 +0000

    new version with elasticsearch in docker

--- data/extract_podcasts.py
+++ data/extract_podcasts.py
@@ -14,6 +14,7 @@ import time
 import re
 import pickle
 from tqdm import tqdm
+import json
 pd.set_option('display.max_columns', None)
 
 
@@ -33,25 +34,27 @@ def url_extraction(url):
     
     
 cat_urls = url_extraction('https://podcasts.apple.com/us/genre/podcasts/id26')
-cat_urls = cat_urls[27:137]
+#print("cat_urls",cat_urls)
+#cat_urls = cat_urls[27:137]
 
 working_urls = []
 
 for i in cat_urls:
-    data = requests.get(i)
-    soup = BeautifulSoup(data.content,'html.parser')
-    s1 = soup.find_all('div',{'class': 'column first'})[0].find_all('a')
-    s2 = soup.find_all('div',{'class': 'column'})[0].find_all('a')
-    s3 = soup.find_all('div',{'class': 'column last'})[0].find_all('a')
-    s = s1+s2+s3
-    urls_within_cat = []
-    for ii in s:
-        urls_within_cat.append(ii.get('href'))
-    working_urls.extend(urls_within_cat)
-    
-   
+    #print("i",i)
+    if "https://podcasts.apple.com/us/genre/podcasts-" in i:
+        data = requests.get(i)
+        soup = BeautifulSoup(data.content,'html.parser')
+        s1 = soup.find_all('div',{'class': 'column first'})[0].find_all('a')
+        s2 = soup.find_all('div',{'class': 'column'})[0].find_all('a')
+        s3 = soup.find_all('div',{'class': 'column last'})[0].find_all('a')
+        s = s1+s2+s3
+        urls_within_cat = []
+        for ii in s:
+            urls_within_cat.append(ii.get('href'))
+        working_urls.extend(urls_within_cat)
 
-delta = 1000
+delta = 100000
+#delta = 2 #test
 urls = working_urls[my_rank*delta:(my_rank+1)*delta]
 
 
@@ -137,47 +140,25 @@ class Extract():
 import time
 start = time.time()
 
-ids = []
-titles = []
-studios = []
-categories = []
-avg_ratings = []
-ratings_volume = []
-episodes_count = []
-description = []
-
-
+podcasts_info =  []
 
 for url in tqdm(urls):
     p = Extract(url)
-    
-    ids.append(p.get_id())
-    titles.append(p.get_title())
-    studios.append(p.get_studio())
-    categories.append(p.get_category())   
-    avg_ratings.append(p.get_avg_rating_and_volume()[0])    
-    ratings_volume.append(p.get_avg_rating_and_volume()[1]) 
-    episodes_count.append(p.get_episode_count())
-    description.append(p.get_description())
-    
+    podcasts_info.append({
+        'id':p.get_id(),
+        'name': p.get_title(),
+        'url': url,
+        'studio': p.get_studio(),
+        'category': p.get_category(),
+        'episode_count': p.get_episode_count(),
+        'avg_rating': p.get_avg_rating_and_volume()[0],
+        'total_ratings': p.get_avg_rating_and_volume()[1],
+        'description' : p.get_description()
+    })
     print(url)
     
 end = time.time()
 print(end - start)
 
-
-
-df = pd.DataFrame(
-        {'id': ids,
-        'name' : titles,
-        'url': urls,
-        'studio': studios,
-        'category' : categories,
-        'episode_count' : episodes_count,
-        'avg_rating' : avg_ratings,
-        'total_ratings' : ratings_volume,
-        'description' : description 
-        })
-
-
-df.to_csv('podcasts_%s.csv' %(my_rank),index=False)
+with open('podcasts.json', 'wt') as f_out:
+    json.dump(podcasts_info, f_out, indent=2)
